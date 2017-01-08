f0d047f8d2d75464d46c8d1407812df155c90175
Austen Adler <stonewareslord@gmail.com>
2017-01-08 01:09:16 -0500

    Updated autoformat script

--- autoformat.sh
+++ autoformat.sh
@@ -1,29 +1,7 @@
 #!/bin/zsh
 # For support:
-# https://austenwares.com/gogs/stonewareslord/autoformat/src/java
+# https://austenwares.com/gogs/stonewareslord/autoformat
 #
-# Format all java files
-GLOB=( **/*.java )
-# Astyle options:
-# --mode=java    : java formatting
-# -xc            : Brace attached to class names
-# --style=google : google style (similar to 1tbs)
-# -j             : Always add brackets (even on one line if statements)
-# -s2            : Three spaces
-# -xG            : Indent modifiers
-# -xe            : Erase blank lines
-# -S             : Indent switches
-# -K             : Indent cases
-# -N             : Indent namespaces
-# -xn            : Attach bracket to namespace
-# -xl            : Attach inlines
-# -n             : Don't make a backup
-# -p             : Pad operators
-# -H             : Pad header (space after if, for, while)
-OPTS=( --mode=java -xc --style=google -j -s2 -xG -xe -S -K -N -xn -xl -n -p -H )
-# Colorize output if you can
-if which colout>/dev/null; then
-  astyle $OPTS $GLOB|\grep -P '^(?!Unchanged)'|colout '(Formatted)' green||true
-else
-  astyle $OPTS $GLOB|\grep -P '^(?!Unchanged)'||true
-fi
+GLOB=( src/**/*.java )
+OPTS=( -H -K -N -S -j -n -p -s2 -xG -xc -xe -xl -xn -z2 --mode=java )
+astyle $OPTS $GLOB|\grep -P '^(?!Unchanged)'||true
