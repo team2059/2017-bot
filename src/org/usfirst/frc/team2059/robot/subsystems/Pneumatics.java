25a790c1d5d1dcd79e540eddcc11f54aa650136e
Achintya Kumar <achintyasi@gmail.com>
2017-01-19 19:10:06 -0500

    added pneumatics

---
+++ src/org/usfirst/frc/team2059/robot/subsystems/Pneumatics.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team2059.robot.subsystems;
+import org.usfirst.frc.team2059.robot.RobotMap;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Compressor;
+
+public class Pneumatics extends Subsystem {
+  Compressor compressor = new Compressor (RobotMap.pcm);
+  DoubleSolenoid gearSolenoid = new DoubleSolenoid (RobotMap.pcm, RobotMap.gearsolenoid1, RobotMap.gearsolenoid2);
+  boolean gearState;
+  public void initDefaultCommand() {
+  }
+  public void setCompressorEnabled(boolean state){
+    compressor.setClosedLoopControl(state);
+  }
+  public void setGearState (boolean state){
+    if (state) {
+      gearSolenoid.set(DoubleSolenoid.Value.kForward);
+    } else {
+      gearSolenoid.set(DoubleSolenoid.Value.kReverse);
+    }
+    gearState = state;
+  }
+public boolean getgearState() {
+    return gearState;
+  }
+public boolean getCompressorEnabled() {
+    return compressor.enabled();
+  }
+}
