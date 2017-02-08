0d211396508d0799eff7d430eedb09a68aa37b3f
F35freak <mtprinceblue@hotmail.com>
2017-02-07 19:08:39 -0500

    Fixed the robotmap,all motors added shooter feeder and 2nd climber motor

--- src/org/usfirst/frc/team2059/robot/RobotMap.java
+++ src/org/usfirst/frc/team2059/robot/RobotMap.java
@@ -7,6 +7,7 @@ public class RobotMap {
   public static int driveRightFrontMotor = 4;
   public static int driveRightRearMotor = 2;
   public static int climbermotor = 5;
+  public static int climbermotor2=6;
   public static int xEncoderA = 2;
   public static int xEncoderB = 3;
   public static int yEncoderA = 0;
@@ -15,17 +16,18 @@ public class RobotMap {
   public static int gearsolenoid1 = 0;
   public static int gearsolenoid2 = 1;
   public static int pcm = 31;
-  public static int collectorMotor = 6;
+  public static int collectorMotor = 7;
   public static int sonar = 1;
-  public static int gearMotor = 7;
-  public static int shooterMotor=8;
+  public static int gearMotor = 8;
+  public static int shooterMotor=9;
+  public static int shooterfeeder=10;
   public static double imageWidth = 320;
   public static double imageHeight = 240;
   public static double fWidth = 483.467261958;
   public static double fHeight = 362.600446468;
   public static int CIMEncoderA = 4;
   public static int CIMEncoderB = 5;
-  public static int ShooterDeflector=9;
+  public static int ShooterDeflector=11;
   public static int ShooterSwitch1=6;
   public static int ShooterSwitch2=7;
   
