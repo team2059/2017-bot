1da2f8cc98d628994f86effec33f50305366b3b1
Achintya Kumar <achintyasi@gmail.com>
2017-02-20 18:30:59 -0500

    fixed angle buttons

---
+++ src/org/usfirst/frc/team2059/robot/commands/SetCameraPosition.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team2059.robot.commands;
+import org.usfirst.frc.team2059.robot.subsystems.VisionHelper;
+
+public class SetCameraPosition extends CommandBase{
+  double anglex;
+  double angley;
+
+  public SetCameraPosition(double x, double y) {
+    anglex = x;
+    angley = y;
+  }
+
+  protected void initialize(){
+  }
+
+  protected void execute(){
+    visionHelper.setServoAngleX(anglex);
+    visionHelper.setServoAngleY(angley);
+
+  }
+
+  protected boolean isFinished() {
+    return false;
+  }
+
+  protected void end() {
+  }
+
+  protected void interrupted() {
+    end();
+  }
+}
