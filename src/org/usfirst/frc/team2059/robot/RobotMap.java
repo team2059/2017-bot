e7ca849f92cf1d5e11571780fb612f17924b319c
Adam Long <macadam.long@gmail.com>
2017-02-25 15:37:15 -0500

    added climberswitches to subsystem

--- src/org/usfirst/frc/team2059/robot/RobotMap.java
+++ src/org/usfirst/frc/team2059/robot/RobotMap.java
@@ -6,23 +6,16 @@ public class RobotMap {
   public static int driveLeftRearMotor = 3;
   public static int driveRightFrontMotor = 4;
   public static int driveRightRearMotor = 2;
-  public static int climbermotor = 5;
-  public static int climbermotor2=6;
   public static int xEncoderA = 2;
   public static int xEncoderB = 3;
   public static int yEncoderA = 0;
   public static int yEncoderB = 1;
   public static int gyro = 0;
-  public static int gearsolenoid1 = 0;
-  public static int gearsolenoid2 = 1;
   public static int pcm = 31;
   public static int collectorMotor = 7;
   public static int sonar = 1;
   public static int gearCollectorMotor = 8;
-  public static double imageWidth = 320;
-  public static double imageHeight = 240;
-  public static double fWidth = 483.467261958;
-  public static double fHeight = 362.600446468;
+
   //Shooter
   public static int ShooterDeflector=12;
   public static int agitatorMotor = 11;
@@ -39,5 +32,17 @@ public class RobotMap {
 
   public static int feederOpenDegrees = 75;
   public static int feederCloseDegrees = 0;
+  
+  //Camera
+  public static double imageWidth = 320;
+  public static double imageHeight = 240;
+  public static double fWidth = 483.467261958;
+  public static double fHeight = 362.600446468;
+
+  //Climber
+  public static int climbermotor = 5;
+  public static int climbermotor2=6;
+  public static int climberSwitch1 = 6;
+  public static int climberSwitch2 = 7;
 
 }
--- src/org/usfirst/frc/team2059/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team2059/robot/subsystems/Climber.java
@@ -2,15 +2,24 @@ package org.usfirst.frc.team2059.robot.subsystems;
 import org.usfirst.frc.team2059.robot.RobotMap;
 import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Climber extends Subsystem {
   CANTalon climberMotor = new CANTalon(RobotMap.climbermotor);
   CANTalon climberMotor2 = new CANTalon(RobotMap.climbermotor2);
+  DigitalInput climberSwitch1 = new DigitalInput(RobotMap.climberSwitch1);
+  DigitalInput climberSwitch2 = new DigitalInput(RobotMap.climberSwitch2);
 
   public void initDefaultCommand() {
   }
   public void climbAtSpeed(double speed) {
     climberMotor.set(speed);
     climberMotor2.set(-speed);
+  }
+  public boolean getClimberSwitch1(){
+    return climberSwitch1.get();
+  }
+  public boolean getClimberSwitch2(){
+    return climberSwitch2.get();
   }
 }
