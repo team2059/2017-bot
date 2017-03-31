1bc42e26a18fc2480350b9f92afec92137d3a3f5
Adam Long <macadam.long@gmail.com>
2017-03-30 20:51:30 -0400

    more changes for states

--- src/org/usfirst/frc/team2059/robot/Robot.java
+++ src/org/usfirst/frc/team2059/robot/Robot.java
@@ -43,8 +43,8 @@ public class Robot extends IterativeRobot {
     SmartDashboard.putNumber("xEncoderTarget", 0);
     SmartDashboard.putNumber("yEncoderTarget", 0);
     SmartDashboard.putNumber("shooterPower", 0.9);
-    SmartDashboard.putNumber("AutoXTarget", 50);
-    SmartDashboard.putNumber("AutoYTarget", 50);
+    SmartDashboard.putNumber("AutoXTarget", -48);
+    SmartDashboard.putNumber("AutoYTarget", 94);
     SmartDashboard.putNumber("driveStraightxkP", 0.2);
     SmartDashboard.putNumber("driveStraightxkD", 0.003);
     SmartDashboard.putNumber("driveStraightxkI", 0.003);
--- src/org/usfirst/frc/team2059/robot/commands/auto/RoutineLeftGearPeg.java
+++ src/org/usfirst/frc/team2059/robot/commands/auto/RoutineLeftGearPeg.java
@@ -6,17 +6,20 @@ import org.usfirst.frc.team2059.robot.Robot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class RoutineLeftGearPeg extends CommandGroup {
   public RoutineLeftGearPeg() {
-//    //addSequential(new AutoEncoderY(93,4));
+    //addSequential(new AutoEncoderY(93,4));
 //    addSequential(new AutoEncoderY(40, 3));
 //    addSequential(new AutoRotate(45, 1.5));
 //    addSequential(new AutoEncoderY(40, 2));
 //    addSequential(new AutoDriveIntoPeg(35, 2));
-//    addSequential(new AutoDriveIntoPeg(40, 2));
 //    addSequential(new AutoEncoderY(-75, 3));
 //    addSequential(new AutoRotate(-45, 2));
-    addSequential(new AutoEncoderY(60,1));
-    addSequential(new AutoRotate(45,1));
-    addSequential(new AutoGoToCoords(SmartDashboard.getNumber("AutoXTarget"),SmartDashboard.getNumber("AutoYTarget"),7)); //test
-    addSequential(new AutoDriveIntoPeg(60, 4));
+//    addSequential(new AutoEncoderY(60,1));
+//    addSequential(new AutoRotate(45,1));
+//    addSequential(new AutoDriveIntoPeg(60, 4));
+
+    addSequential(new AutoEncoderY(20, 2));
+    addSequential(new AutoRotate(60, 2));
+    addSequential(new AutoGoToCoords(SmartDashboard.getNumber("AutoXTarget"),SmartDashboard.getNumber("AutoYTarget"),5)); //test
+    addSequential(new AutoDriveIntoPeg(20, 4));
   }
 }
