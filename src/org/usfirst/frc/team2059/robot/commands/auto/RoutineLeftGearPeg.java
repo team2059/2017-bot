e2ed703bb94c6eb783221a2b4eba64ca9555cc27
Adam Long <macadam.long@gmail.com>
2017-04-02 12:52:13 -0400

    updated left gear side auto

--- src/org/usfirst/frc/team2059/robot/commands/auto/RoutineLeftGearPeg.java
+++ src/org/usfirst/frc/team2059/robot/commands/auto/RoutineLeftGearPeg.java
@@ -6,10 +6,10 @@ import org.usfirst.frc.team2059.robot.Robot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class RoutineLeftGearPeg extends CommandGroup {
   public RoutineLeftGearPeg() {
-    addSequential(new AutoGoToCoords(20,0,1)); //test
-    addSequential(new AutoRotate(-30, 2));
-    addSequential(new AutoGoToCoords(48,94,4)); //test
-    addSequential(new AutoDriveIntoPeg(25, 3));
+    addSequential(new AutoSetGearAngle(0, .1));
+    addSequential(new AutoEncoderY(70, 3));
+    addSequential(new AutoRotate(60, 2.5));
+    addSequential(new AutoEncoderY(90, 3));
     addParallel(new AutoSetGearAngle(-36, .5));
     addSequential(new AutoEncoderY(-40, 3));
   }
